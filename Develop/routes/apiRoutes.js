5bf08710998c2e8d401800303ca3fb1c8b4e8a43
batwood99 <atwood@mail.usf.edu>
2023-06-25 12:33:04 -0400

    added api get

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,3 +1,11 @@
 const router = require('express').Router();
-const store = require('../db/store');
 
+router.get('/notes', (req, res) => {
+    store
+      .getNotes()
+      .then((notes) => {
+        return res.json(notes);
+      })
+      .catch((err) => res.status(500).json(err));
+  });
+  
