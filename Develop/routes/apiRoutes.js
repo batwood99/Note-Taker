7bed60fcee8b605772065cc732aa01eaebf11963
batwood99 <atwood@mail.usf.edu>
2023-06-25 12:37:58 -0400

    finished apiRoutes and added db store file

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -8,4 +8,19 @@ router.get('/notes', (req, res) => {
       })
       .catch((err) => res.status(500).json(err));
   });
+  router.post('/notes', (req, res) => {
+    store
+      .addNote(req.body)
+      .then((note) => res.json(note))
+      .catch((err) => res.status(500).json(err));
+  });
+  
+  router.delete('/notes/:id', (req, res) => {
+    store
+      .removeNote(req.params.id)
+      .then(() => res.json({ ok: true }))
+      .catch((err) => res.status(500).json(err));
+  });
+  
+  module.exports = router;
   
